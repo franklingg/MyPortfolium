41b3fad13d9006bd1108d6e604d1910c268bab17
franklingg <franklin.oliveira@ccc.ufcg.edu.br>
2021-07-28 10:54:08 -0300

    TS and Next config files, folder structure

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+module.exports = {
+	useFileSystemPublicRoutes: false,
+	async redirects() {
+		return [
+			{
+				source: '/home',
+				destination: '/',
+				permanent: true
+			}
+		];
+	}
+};
+
+const withImages = require('next-images');
+
+module.exports = withImages({
+	esModule: true
+});
