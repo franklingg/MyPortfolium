392142a8a6d5fb4ef36f17438fe51f22ffc94fa4
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2021-11-28 14:37:18 -0300

    adiciona página de not found

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,18 @@
+import { Container, Row } from "react-bootstrap";
+import Image from "next/image";
+
+import styles from './404.module.css';
+import Banner404 from '~/assets/404.svg';
+
+export default function Error404() {
+
+  return (
+    <Container fluid className={styles.error}>
+      <Row className={styles.error__text}>
+        <h2>Por aqui não!</h2>
+        <p>Esta página ainda não existe, tente por outro caminho</p>
+      </Row>
+      <Image src={Banner404} alt="Imagem de erro retirada de https://storyset.com" width={450} height={450} />
+    </Container>
+  );
+}
