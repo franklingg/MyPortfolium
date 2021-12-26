9225b7bc8fa32a6a6bec8dc6ee9dca4c5fcdcbd9
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2021-12-26 13:40:59 -0300

    adiciona form de contato

--- src/components/ContactButton/ContactButton.tsx
+++ src/components/ContactButton/ContactButton.tsx
@@ -1,16 +1,47 @@
-import { Row } from "react-bootstrap";
-import Image from "next/image";
+import { useCallback, useState, useRef, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
 
 import styles from "./ContactButton.module.css";
 import { useLangContext } from "~/contexts/langContext";
+import { SimpleButton } from "..";
 
 export default function ContactButton() {
 
+  const [ formOpen, setFormOpen ] = useState(false);
+  const formRef = useRef<HTMLDivElement>(null);
   const { pageContent } = useLangContext();
 
+  const handleOpenForm = useCallback(() => { setFormOpen(true) }, []);
+  const handleCloseForm = useCallback(() => { setFormOpen(false)}, []);
+
+  const handleClickOutside = useCallback((event: MouseEvent ) => {
+    if (!formRef.current?.contains(event.target as HTMLDivElement)) {
+      handleCloseForm();
+    }
+  }, [handleCloseForm]);
+
+  useEffect(()=>{
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  });
+
   return (
-    <button className={styles.contactButton}>
-      {pageContent['contactButton']}
-    </button>
+    !formOpen ? (
+      <button className={`${styles.fixedContainer} ${styles.contactButton}`} onClick={handleOpenForm}>
+        {pageContent['contactButton']}
+      </button>
+    ) : (
+      <Container className={`${styles.fixedContainer} ${styles.contactForm}`} ref={formRef}>
+        <h4 className={styles.contactForm__title}>
+          Vamos conversar sobre o seu projeto?
+        </h4>
+        <Row className={styles.contactForm__inputs}>
+          <input />
+        </Row>
+        <button className={styles.contactForm__send}> 
+          Enviar 
+        </button>
+      </Container>
+    )
   );
 }
